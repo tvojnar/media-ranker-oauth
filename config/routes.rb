f4d1fb7aa220454a8289d69788d2d5a37ac38f19
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-16 11:54:32 -0700

    Add callback rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#root'
-  get '/login', to: 'sessions#login_form', as: 'login'
-  post '/login', to: 'sessions#login'
-  post '/logout', to: 'sessions#logout', as: 'logout'
+  # get '/login', to: 'sessions#login_form', as: 'login'
+  # post '/login', to: 'sessions#login'
+  # post '/logout', to: 'sessions#logout', as: 'logout'
+
+  get '/auth/:provider/callback', to: 'users#login'
 
   resources :works
   post '/works/:id/upvote', to: 'works#upvote', as: 'upvote'
