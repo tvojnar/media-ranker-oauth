5741a9c704875f794950bdb8b38757520392d6a2
Tamira Vojnar <tamiravojnar@gmail.com>
2017-10-16 12:32:11 -0700

    Made save_flash method in application_controller and am partway through log in action in sessions_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,29 @@ class ApplicationController < ActionController::Base
 
   before_action :find_user
 
+
+
+
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
   end
+  
+protected
+def save_flash(model)
+  saved = model.save
+
+  if saved
+    flash[:status] = :success
+    flash[:message] = "Successfully created a new user, #{model.name}"
+  else
+    flash[:status] = :success
+    flash[:message] = "A problem occurred: Could not create a new user}"
+    flash[:details] = model.errors.messages
+  end # if/else
+
+  return saved
+end # save and flash
 
 private
   def find_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,32 +3,48 @@ class SessionsController < ApplicationController
   end
 
   def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
-      end
-    end
-    redirect_to root_path
-  end
+    auth_hash = request.env['omniauth.auth']
 
-  def logout
-    session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
-    redirect_to root_path
-  end
+    if auth_hash['uid']
+      user = User.find_by(provider: params[:provider ], uid: auth_hash['uid'])
+      if user.nil?
+        # create a new user using a method in the User model
+        user = User.from_auth_hash(params[:provider], auth_hash)
+        save_flash(user)
+        session[:user_id] = user.id
+      else
+        session[:user_id] = user.id
+        # TODO: Continue writing this message after lunch!
+  end # login
+
+# NOTE: below is old log in and log out methods for just using session
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     user = User.new(username: username)
+  #     if user.save
+  #       session[:user_id] = user.id
+  #       flash[:status] = :success
+  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  #     else
+  #       flash.now[:status] = :failure
+  #       flash.now[:result_text] = "Could not log in"
+  #       flash.now[:messages] = user.errors.messages
+  #       render "login_form", status: :bad_request
+  #       return
+  #     end
+  #   end
+  #   redirect_to root_path
+  # end
+  #
+  # def logout
+  #   session[:user_id] = nil
+  #   flash[:status] = :success
+  #   flash[:result_text] = "Successfully logged out"
+  #   redirect_to root_path
+  # end
 end
